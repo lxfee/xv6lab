b0a6db4c631ebfbbbf31344b8b2c80e1428b66d0
limil <limil.top@qq.com>
2022-05-26 15:15:00 +0800

    exp2 done

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf(s);
   printf("\n");
   panicked = 1; // freeze uart output from other CPUs
+  backtrace();
   for(;;)
     ;
 }
@@ -131,4 +132,20 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace()
+{
+  uint64 fp = r_fp();
+  uint64 up = PGROUNDUP(fp);
+  uint64 down = PGROUNDDOWN(fp);
+  printf("backtrace:\n");
+  
+  while(1) {
+    uint64 nfp = *(uint64*)(fp - 16);
+    if(nfp > up || nfp < down) break;
+    printf("%p\n", *((uint64*)(fp - 8)) - 8);
+    fp = nfp;
+  }
 }
